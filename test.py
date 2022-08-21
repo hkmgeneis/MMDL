528baa3e0c193ed5d952f9465940e8b986c10183
hkmgeneis <106580890+hkmgeneis@users.noreply.github.com>
2022-08-21 10:48:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 data_transforms = {
     'test': transforms.Compose([
         transforms.Resize(256),
-        transforms.CenterCrop(224), # 从中心裁剪
+        transforms.CenterCrop(224), 
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
