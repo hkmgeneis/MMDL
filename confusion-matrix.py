937d9fe5dbcb7316529c717762c3d1e162684f79
hkmgeneis <106580890+hkmgeneis@users.noreply.github.com>
2022-08-29 16:44:31 +0800

    Update confusion-matrix.py

--- confusion-matrix.py
+++ confusion-matrix.py
@@ -8,24 +8,19 @@ __Author__: xisx
 """
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.metrics import confusion_matrix, roc_curve, auc, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
 import seaborn as sns
 from scipy import interp
 from itertools import cycle
 from utils.common import logger
 import argparse
-
 import torch
 import os
-
 import sys
 sys.path.append('..')
-from test import test
 
-import numpy as np
-import pandas as pd
-y_pred0=np.load('/home/zyj/Desktop/hkm/code/mmdl/results/clinic/y_pred_0.npy')
-y_pred1=np.load('/home/zyj/Desktop/hkm/code/mmdl/results/clinic/y_pred_1.npy')
+y_pred0=np.load(os.getcwd() + f'/results/y_pred_0.npy')
+y_pred1=np.load(os.getcwd() + f'/results/y_pred_1.npy')
 #y_pred2=np.load('/home/ubuntu/hkm/results/tmb5/top20/top201/y_pred_2.npy')
 #y_pred3=np.load('/home/ubuntu/hkm/results/tmb5/top20/top201/y_pred_3.npy')
 #y_pred4=np.load('/home/ubuntu/hkm/results/tmb5/top20/top201/y_pred_4.npy')
@@ -35,10 +30,10 @@ predict=np.append(y_pred0,y_pred1)
 #predict=np.append(predict,y_pred3)
 #predict=np.append(predict,y_pred4)
 predict=np.array(predict)
-np.save("/home/zyj/Desktop/hkm/code/mmdl/results/clinic/pred.npy",predict)
+np.save(os.getcwd() + f'/results/pred.npy',predict)
 
-y_true0=np.load('/home/zyj/Desktop/hkm/code/mmdl/results/clinic/y_true_0.npy')
-y_true1=np.load('/home/zyj/Desktop/hkm/code/mmdl/results/clinic/y_true_1.npy')
+y_true0=np.load(os.getcwd() + f'/results/y_true_0.npy')
+y_true1=np.load(os.getcwd() + f'/results/y_true_1.npy')
 #y_true2=np.load('/home/ubuntu/hkm/results/tmb5/top20/top201/y_true_2.npy')
 #y_true3=np.load('/home/ubuntu/hkm/results/tmb5/top20/top201/y_true_3.npy')
 #y_true4=np.load('/home/ubuntu/hkm/results/tmb5/top20/top201/y_true_4.npy')
@@ -48,11 +43,9 @@ true=np.append(y_true0, y_true1)
 #true=np.append(true,y_true3)
 #true=np.append(true,y_true4)
 true=np.array(true)
-np.save("/home/zyj/Desktop/hkm/code/mmdl/results/clinic/true.npy",true)
-
+np.save(os.getcwd() + f'/results/true.npy',true)
 
 # plot confusion-matrix
-#def plot_confusion_matrix(labels, pred, title='confusion matrix', address='/home/ubuntu/hkm/tmbpredictor-master/results/rec/rec2/confusion_matrix.svg'):
 sns.set()
 f, ax = plt.subplots(figsize=(9,6))
 c2 = confusion_matrix(np.array(predict),np.array(true))
@@ -61,7 +54,7 @@ ax.set_title('confusion matric')
 ax.set_xlabel('true')
 ax.set_ylabel('predict')     
 plt.show()
-plt.savefig('/home/zyj/Desktop/hkm/code/mmdl/results/clinic/cm-tmb2.svg')
+plt.savefig(os.getcwd() + f'/results/cm-tmb2.svg')
     
 p = precision_score(true,predict, average='macro')
 r = recall_score(true,predict, average='macro')
